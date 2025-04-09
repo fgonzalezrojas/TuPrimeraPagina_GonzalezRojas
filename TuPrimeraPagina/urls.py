192c10324ff95764bc738c5bf069c98fb44335d7
Fernando Gonzalez Rojas <fernandodgr@hotmail.com>
2025-04-09 15:14:41 -0300

    Paso 2: Creacion de la app vinoteca

--- TuPrimeraPagina/urls.py
+++ TuPrimeraPagina/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from vinoteca.views import vinoteca
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("vinoteca", vinoteca)
 ]
