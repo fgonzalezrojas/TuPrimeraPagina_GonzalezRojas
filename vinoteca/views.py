b36576bb6c6f047e0e5df0c9649024065fde8079
Fernando Gonzalez Rojas <fernandodgr@hotmail.com>
2025-04-10 10:50:08 -0300

    Paso 7: herencia de templates desde 00 home.html

--- vinoteca/views.py
+++ vinoteca/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def vino(request):
-    return render(request, "vinoteca/vino.html")
+    return render(request, "vinoteca/01 vino.html")
 
 def whisky(request):
-    return render(request, "vinoteca/whisky.html")
+    return render(request, "vinoteca/02 whisky.html")
 
 def champagne(request):
-    return render(request, "vinoteca/champagne.html")
+    return render(request, "vinoteca/03 champagne.html")
 
