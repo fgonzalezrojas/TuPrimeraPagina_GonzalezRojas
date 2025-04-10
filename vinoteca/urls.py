e2d2651b4b1f8dcbc887596b6be91f54772a2a3f
Fernando Gonzalez Rojas <fernandodgr@hotmail.com>
2025-04-10 11:30:01 -0300

    Paso 8: creacion app_name en vinoteca/urls.py y reemplazo de url en 00 home.html

--- vinoteca/urls.py
+++ vinoteca/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from vinoteca.views import vino , whisky , champagne
 
+app_name = "vinoteca"
+
 urlpatterns = [
         path("vino", vino , name = "vino"),
         path("whisky", whisky , name = "whisky"),
