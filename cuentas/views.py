7ae9601a8e0071cf78770c5f1a6401606170f14e
Fernando Gonzalez Rojas <fernandodgr@hotmail.com>
2025-04-18 17:41:46 -0300

    Paso 23: correccion OK accceso a traves de login/ cuentas/viewa.py

--- cuentas/views.py
+++ cuentas/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth.models import User
 
 from django.contrib.auth import logout
 from django.shortcuts import redirect
@@ -19,7 +18,12 @@ class UserRegisterView(CreateView):
     template_name = "cuentas/00 signup.html"
     form_class = UserCreationForm
     success_url = reverse_lazy("cuentas:login")
-
 class UserLoginView(LoginView):
     template_name = "cuentas/01 login.html"
+    success_url = reverse_lazy("vinoteca:home")
+
+    def get_success_url(self):  # Agrega este método
+        return self.success_url
+    
+
 
